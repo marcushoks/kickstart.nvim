996c0683bf5c423e2f8bbef4efda26c16b22f00e
Marcus Ho <marcushoks@gmail.com>
2025-03-05 14:40:03 +0800

    Add eslint and prettier

--- init.lua
+++ init.lua
@@ -711,6 +711,11 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        prettierd = {},
+
+        eslint_d = {},
+
+
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -799,7 +804,9 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'eslint_d', stop_after_first = true },
+        typescript = { 'prettierd', 'eslint_d', stop_after_first = true },
+        vue = { 'prettierd', 'eslint_d', stop_after_first = true },
       },
     },
   },
