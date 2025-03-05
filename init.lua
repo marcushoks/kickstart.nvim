fefef1766a7039e6cdc26de44875e6887ce7bb40
Marcus Ho <marcushoks@gmail.com>
2025-03-05 14:40:03 +0800

    Add prettier

--- init.lua
+++ init.lua
@@ -824,7 +824,9 @@ do
       -- python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      vue = { "prettierd", "prettier", stop_after_first = true },
     },
   }
 
