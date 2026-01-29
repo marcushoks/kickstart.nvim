eda19fab2e56d2fdea3a08fd54fa7d8405adbf3f
Marcus <marcushoks@gmail.com>
2026-01-29 10:44:36 +0800

    Enable telescope colorscheme preview

--- init.lua
+++ init.lua
@@ -522,7 +522,9 @@ do
     --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
     --   },
     -- },
-    -- pickers = {}
+    pickers = {
+      colorscheme = { enable_preview = true },
+    },
     extensions = {
       ['ui-select'] = { require('telescope.themes').get_dropdown() },
     },
