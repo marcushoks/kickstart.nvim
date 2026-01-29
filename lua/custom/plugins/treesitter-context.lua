845d67c47f3137a264017d6256bbbfabf215a877
Marcus <marcushoks@gmail.com>
2026-01-29 09:42:52 +0800

    Remove treesitter-context underline

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -1,2 +1,3 @@
 vim.pack.add { 'nvim-treesitter/nvim-treesitter-context' }
 require('nvim-treesitter/nvim-treesitter-context').setup {}
+vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { underline = false })
